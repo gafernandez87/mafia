38d4f34155b44e593b9eb125bbf508694f4f8eb5
Gaston Fernandez <gaston.fernandez@softvision.com>
2020-09-02 19:58:32 -0300

    gitignore in clien

---
+++ client/build/precache-manifest.957944da31abe2f4408e4a7277b30da9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78d3f2eb917c25dff565805371eace6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81f0df2473f16b974d2b",
+    "url": "/static/css/main.ffa70d59.chunk.css"
+  },
+  {
+    "revision": "8b50b1eb0875eedeaa0a",
+    "url": "/static/js/2.93ba1ace.chunk.js"
+  },
+  {
+    "revision": "a360305518243deee3bfebd4256d9edd",
+    "url": "/static/js/2.93ba1ace.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81f0df2473f16b974d2b",
+    "url": "/static/js/main.ef14c43a.chunk.js"
+  },
+  {
+    "revision": "6a460fdabcc5765b113b",
+    "url": "/static/js/runtime-main.cd281166.js"
+  }
+]);
